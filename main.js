7d96d160ebcbf2ed443bb8607db9321bcb519014
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2020-07-13 15:37:09 +0200

    impl: whole cmd line sudo

--- main.js
+++ main.js
@@ -5,13 +5,19 @@ const files = [
   "s3_object_exists.sh",
   "s3_uri.sh",
   "s3_url.sh",
-].join(",");
+];
+
+const commas = files.join(",");
+
+const chmodx = files.map(function (file) {
+  return "chmod +x /usr/local/bin/" + file;
+}).join(" && ");
 
 exec(
-  "cd /usr/local/bin\n" +
-  `sudo curl -fSL# --remote-name-all https://raw.githubusercontent.com/chiefbiiko/aws-actions-cheats/master/cheats/{${files}}\n` +
-  "chmod +x /usr/local/bin/s3*\n" +
-  "cd $HOME && s3_url -h",
+  "sudo cd /usr/local/bin && " +
+  "curl -fSL# --remote-name-all https://raw.githubusercontent.com/chiefbiiko/aws-actions-cheats/master/cheats/{" + commas + "} && " +
+  chmodx +
+  " && cd $HOME && s3_url -h",
   function (err) {
     if (err) {
       console.error(err.message);
