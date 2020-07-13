f5d5be68ce4c25cfb5c31d246cdab6b0e2d63846
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2020-07-13 15:41:43 +0200

    impl: sudo sh -c '...'

--- main.js
+++ main.js
@@ -14,10 +14,10 @@ const chmodx = files.map(function (file) {
 }).join(" && ");
 
 exec(
-  "sudo cd /usr/local/bin && " +
+  "sudo sh -c 'cd /usr/local/bin && " +
   "curl -fSL# --remote-name-all https://raw.githubusercontent.com/chiefbiiko/aws-actions-cheats/master/cheats/{" + commas + "} && " +
   chmodx +
-  " && cd $HOME && s3_url -h",
+  " && cd $HOME && s3_url -h'",
   function (err) {
     if (err) {
       console.error(err.message);
