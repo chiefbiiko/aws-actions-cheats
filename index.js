5e08f4c620afd7b616e2091f216578f8ec31e97a
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2020-07-13 13:31:58 +0200

    impl: now sh sourcing with a dot

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const exec = require('@actions/exec').exec;
 
 async function main() {
   try {
-    await exec('source ./cheats.sh');
+    await exec('. ./cheats.sh');
   } catch(err) {
-    core.setFailed(err.message);
+    return core.setFailed(err.message);
   }
   core.info('aws-actions-cheats successfully sourced');
 }
