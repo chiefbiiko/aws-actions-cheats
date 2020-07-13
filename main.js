5ec57e1287178735bfecf0bdfe974142a1545cfc
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2020-07-13 16:11:08 +0200

    impl: toss sh file extensions

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 const exec = require("child_process").exec;
 
 const files = [
-  "s3_bucket_exists.sh",
-  "s3_object_exists.sh",
-  "s3_uri.sh",
-  "s3_url.sh",
+  "s3_bucket_exists",
+  "s3_object_exists",
+  "s3_uri",
+  "s3_url",
 ];
 
 const commas = files.join(",");
 
 const chmodx = files.map(function (file) {
-  return "chmod +x /usr/bin/" + file;
+  return "chmod +x /usr/local/bin/" + file;
 }).join(" && ");
 
-const cmd = "sudo sh -c 'cd /usr/bin && " +
+const cmd = "sudo sh -c 'cd /usr/local/bin && " +
   "curl -fSL# --remote-name-all https://raw.githubusercontent.com/chiefbiiko/aws-actions-cheats/master/cheats/{" +
   commas + "} && " +
   chmodx + "'";
