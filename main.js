bec5c1f17f8e6c91fb25b1cb35d22e7ec56001f6
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2020-07-13 14:07:00 +0200

    debug: using env vars to get pwd

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const exec =  require("child_process").exec;
 
-exec("source ./cheats.sh", { shell: "/bin/bash" }, function (err) {
-  if (err) {
-    console.error(err.message);
-    process.exitCode = 1;
-  } else {
-    console.log("aws-actions-cheats are now available in your sh");  
-  }
+exec(
+  "ls && source $GITHUB_WORKSPACE/aws-actions-cheats/cheats.sh",
+  { shell: "/bin/bash" },
+  function (err) {
+    if (err) {
+      console.error(err.message);
+      process.exitCode = 1;
+    } else {
+      console.log("aws-actions-cheats are now available in your sh");  
+    }
 });
