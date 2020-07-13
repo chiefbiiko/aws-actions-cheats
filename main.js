2c983dc42febc644fe9c58ba23d1329221a15f38
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2020-07-13 16:05:10 +0200

    debug: func not found

--- main.js
+++ main.js
@@ -13,16 +13,16 @@ const chmodx = files.map(function (file) {
   return "chmod +x /usr/bin/" + file;
 }).join(" && ");
 
-exec(
-  "sudo sh -c 'cd /usr/bin && " +
-  "curl -fSL# --remote-name-all https://raw.githubusercontent.com/chiefbiiko/aws-actions-cheats/master/cheats/{" + commas + "} && " +
-  chmodx +
-  " && ls /usr/bin | grep s3'",
-  function (err) {
-    if (err) {
-      console.error(err.message);
-      process.exitCode = 1;
-    } else {
-      console.log("aws-actions-cheats are now available in your sh");  
-    }
+const cmd = "sudo sh -c 'cd /usr/bin && " +
+  "curl -fSL# --remote-name-all https://raw.githubusercontent.com/chiefbiiko/aws-actions-cheats/master/cheats/{" +
+  commas + "} && " +
+  chmodx + "'";
+
+exec(cmd, function (err) {
+  if (err) {
+    console.error(err.message);
+    process.exitCode = 1;
+  } else {
+    console.log("aws-actions-cheats are now available in your sh");
+  }
 });
