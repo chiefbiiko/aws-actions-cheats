6f5871ed4b5e72215e47868eaa1e6ba657503772
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2020-07-13 14:01:11 +0200

    impl: try forcing bash shell

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const exec =  require("child_process").exec;
 
-exec(". ./cheats.sh", function (err) {
+exec("source ./cheats.sh", { shell: "/bin/bash" }, function (err) {
   if (err) {
     console.error(err.message);
     process.exitCode = 1;
